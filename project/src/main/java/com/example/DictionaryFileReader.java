1633195c8779d60c69adf14f4766db5270e5890a
Filip Kara <81981908+FilKara@users.noreply.github.com>
2026-06-20 22:10:28 +0200

    initial

--- project/src/main/java/com/example/DictionaryFileReader.java
+++ project/src/main/java/com/example/DictionaryFileReader.java
@@ -1,4 +1,35 @@
-package com.example.patterns;
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class DictionaryFileReader {
+
+    private String filePath;
+    public DictionaryFileReader(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getRawFileData() {
+        StringBuilder rawData = new StringBuilder();
+        File file = new File(filePath);
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                rawData.append(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return rawData.toString();
+
+    }
 }
--- project/src/main/java/com/example/DictionaryValue.java
+++ project/src/main/java/com/example/DictionaryValue.java
@@ -1,4 +1,57 @@
 package com.example;
 
 public class DictionaryValue {
+    private int id;
+    private int intKey;
+    private String stringKey;
+    private String dictionaryName;
+    private String value;
+
+    public DictionaryValue(int id, int intKey, String stringKey, String dictionaryName, String value) {
+        this.id = id;
+        this.intKey = intKey;
+        this.stringKey = stringKey;
+        this.dictionaryName = dictionaryName;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getIntKey() {
+        return intKey;
+    }
+
+    public String getStringKey() {
+        return stringKey;
+    }
+
+    public String getDictionaryName() {
+        return dictionaryName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIntKey(int intKey) {
+        this.intKey = intKey;
+    }
+
+    public void setStringKey(String stringKey) {
+        this.stringKey = stringKey;
+    }
+
+    public void setDictionaryName(String dictionaryName) {
+        this.dictionaryName = dictionaryName;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
